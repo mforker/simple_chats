adf448383a821eaa3b9436d6f8fcdd24ae8b14b4
Mitesh Nandan <nandanmitesh@gmail.com>
2024-11-09 20:13:00 +0530

    geminichatbot

---
+++ app.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import streamlit as st
+import google.generativeai as genai
+import os
+
+load_dotenv()
+
+api = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=api)
+
+# func to load gemini model
+model = genai.GenerativeModel('gemini-1.5-flash')
+chat = model.start_chat(history=[])
+
+def get_response(query):
+    res = chat.send_message(query, stream=True)
+    return res
+
+st.title("Gemini Powered Chatbot")
+
+if 'message' not in st.session_state:
+    st.session_state['message'] = []
+
+for message in st.session_state['message']:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+if prompt:= st.chat_input("Text here"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    st.session_state['message'].append({"role" : "You", "content" : prompt})
+
+    response = get_response(prompt)
+
+    with st.chat_message("Assistant"):
+        for chunks in response:
+            st.markdown(chunks.text)
+
+    st.session_state['message'].append({"role" : "Bot", "content" : response.text})
--- main.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import streamlit as st
-import os
-
-load_dotenv()
-
-api = os.getenv("GEMINI_API_KEY")
-print(api)
